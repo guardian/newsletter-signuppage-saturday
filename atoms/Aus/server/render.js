3128a8830f5e83024d93d8d324068f7b6185a003
Harry Fischer <harry.fischer@guardian.co.uk>
2020-09-24 12:48:56 +0100

    embeds for 1million article:

---
+++ atoms/Aus/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/aus/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+}
---
+++ atoms/Brazil/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/brazil/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+}
