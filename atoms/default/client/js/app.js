24380ccf74e35a54495b7480dfe7e2c57c5b7b4b
Harry Fischer <harry.fischer@guardian.co.uk>
2020-09-30 11:55:40 +0100

    fixed dates

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -12,9 +12,20 @@ const openButtons = document.querySelectorAll(".open-button");
 for (var i = 0; i < openButtons.length; i++) {
   openButtons[i].addEventListener("click", function () {
     var openMe = this.parentNode;
+    if (!this.classList.contains("open")) {
+      for (var j = 0; j < openButtons.length; j++) {
+        const wrapper = document.querySelectorAll(".all-reviews__sections__wrapper")
+        const wrapperBtn = wrapper[j].querySelector(".open-button")
+        wrapper[j].classList.remove("open")
+
+        wrapperBtn.innerHTML = readMore;
+      }
+    }
+
     openMe.classList.toggle("open");
     this.classList.toggle("open");
     console.log(this.classList);
+
     if (this.classList.contains("open")) {
       this.innerHTML = readLess;
     } else {
@@ -106,165 +117,51 @@ window.addEventListener("resize", () => {
   runSwiper();
 });
 
-// restyle spotify
-// const spotifyPlayer = document.querySelector(".all-reviews__sections__song");
-// const spotifyBackground = spotifyPlayer.contentWindow.document.querySelector(
-//     ".at"
-// );
-// spotifyBackground.style.backgroundColor = "#121212";
 
-// header animation
-// const allCovers = document.querySelector(".all-reviews__header__covers");
-// const albumCovers = document.querySelectorAll(
-//     ".all-reviews__header__covers__image"
-// );
 
-// albumCovers.i.addEventListener("mouseover", function () {
-//     albumCovers.forEach((cover) => {
-//         const x = 50 * Math.random() - 25;
-//         const r = 10 * Math.random() - 5;
-//         const y = 50 * Math.random() - 25;
-//         cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
-//     });
-// });
-// let currentCover = 0;
-// let z = 1;
 
-// // move covers on load
-// allCovers.addEventListener("load", loadMove());
 
-// function loadMove() {
-//     albumCovers.forEach((cover) => {
-//         const x = 50 * Math.random() - 25;
-//         const r = 10 * Math.random() - 5;
-//         const y = 50 * Math.random() - 25;
-//         cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
-//     });
-// }
 
-// allCovers.addEventListener("mouseover", function () {
-//     albumCovers.forEach((cover) => {
-//         const x = 50 * Math.random() - 25;
-//         const r = 10 * Math.random() - 5;
-//         const y = 50 * Math.random() - 25;
-//         cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
-//     });
-// });
 
-// allCovers.addEventListener("mouseout", function () {
-//     albumCovers.forEach((cover) => {
-//         const x = 50 * Math.random() - 25;
-//         const r = 10 * Math.random() - 5;
-//         const y = 50 * Math.random() - 25;
-//         cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
-//     });
-// });
 
-// // move covers to top on click
-// allCovers.addEventListener("click", function () {
-//   currentCover = currentCover + 1;
-//   if (currentCover > albumCovers.length - 1) {
-//     currentCover = 0;
+// --if getting the date from an element?:
+const dateElAll = document.querySelectorAll('.publication-date span');
+
+dateElAll.forEach((dateEl) => {
+  const dateString = dateEl.innerHTML;
+
+  const date = new Date(Date.parse(dateString));
+
+  const formattedDate = date.toLocaleString('en-GB', {
+    month: 'short',
+    year: 'numeric',
+    weekday: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short'
+  }).replace(/,/g, '');
+
+  // --to place the formatted date back in element:
+  dateEl.innerText = formattedDate;
+})
+
+
+
+
+
+// lazy load spotify
+
+
+
+// document.addEventListener("scroll", function () {
+//   const spotifyFrame = document.querySelectorAll(".spotify-iframe")
+//   console.log(spotifyFrame)
+//   const offset = spotifyFrame.getBoundingClientRect().top - (window.innerHeight * 2)
+//   console.log(offset)
+//   if (offset < 0) {
+//     spotifyFrame.forEach(() => {
+//       spotifyFrame.setAttribute("src", iframe.dataset.src)
+//     })
 //   }
-//   z = z + 1;
-//   albumCovers[currentCover].style.zIndex = z;
-// });
-
-// import SpotifyWebApi from "spotify-web-api-js";
-
-// //spotifyPlayer
-// var Spotify = require("spotify-web-api-js");
-// var s = new Spotify();
-// var spotifyApi = new SpotifyWebApi();
-
-// spotifyApi.setAccessToken("f19ddabdb3534b66896b149b9ab68780");
-
-// // get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
-// spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", function (err, data) {
-//   if (err) console.error(err);
-//   else console.log("Artist albums", data);
-// });
-
-// // get Elvis' albums, using Promises through Promise, Q or when
-// spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE").then(
-//   function (data) {
-//     console.log("Artist albums", data);
-//   },
-//   function (err) {
-//     console.error(err);
-//   }
-// );
-
-// var prev = null;
-
-// function onUserInput(queryTerm) {
-//   // abort previous request, if any
-//   if (prev !== null) {
-//     prev.abort();
-//   }
-
-//   // store the current promise in case we need to abort it
-//   prev = spotifyApi.searchTracks(queryTerm, { limit: 5 });
-//   prev.then(
-//     function (data) {
-//       // clean the promise so it doesn't call abort
-//       prev = null;
-
-//       // ...render list of search results...
-//     },
-//     function (err) {
-//       console.error(err);
-//     }
-//   );
-// }
-
-// spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", function (err, data) {
-//   if (err) console.error(err);
-//   else console.log("Artist albums", data);
-// });
-
-// // get Elvis' albums, using Promises through Promise, Q or when
-// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-//     function (data) {
-//         console.log('Artist albums', data);
-//     },
-//     function (err) {
-//         console.error(err);
-//     }
-// );
-
-// spotifyApi.searchTracks("Love").then(
-//     function (data) {
-//         console.log('Search by "Love"', data);
-
-// var artistNames = document.querySelectorAll('[[selector]]');
-// var player = document.querySelector('player');
-//
-// for(i = 0; i < artistNames.length; i++) {
-//   if(data.track.artistname === artistNames[i]){
-//     var tracklink = data.track.previewMP4;
-//     player.src = tracklink;
-//   }
-// }
-//     },
-//     function (err) {
-//         console.error(err);
-//     }
-// );
-
-// playbutton.addEventlistner("click", function () {
-// player.src = tracklink;
-//
-// if(!player[i].pause()){
-//   player[i].play();
-// } else {
-//   player[i].pause()
-// }
-// });
-
-// const defaultHeight = "300px";
-// const openedHeight = "100%";
-// const readLess = `<svg width="34" height="17" viewBox="0 0 34 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-// <path d="M0 15.5833L16.2237 3.10933e-07H17.7763L34 15.5833L32.4863 17L17 4.47368L1.5137 17L0 15.5833Z" fill="#ffffff"/>
-// </svg>Read less`;
-// const readMore = `<svg width="34" height="18" viewBox="0 0 34 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M0.0771484 1.7592L16.2271 17.9092H17.7726L33.9226 1.7592L32.4158 0.291016L16.9999 13.2728L1.58397 0.291016L0.0771484 1.7592Z" fill="#ffffff" /></svg>Read more`;
+// })
