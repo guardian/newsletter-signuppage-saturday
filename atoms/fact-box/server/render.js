943101af2feffcf9f89b2a600866d7e2d1c3df4e
blongden73 <hello@benlongdendesign.co.uk>
2020-11-30 17:38:10 +0000

    added giving thrasher

---
+++ atoms/fact-box/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/fact-box/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+}
---
+++ atoms/giving/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/giving/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+}
