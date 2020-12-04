06ec007fba333ea79ad48075cdd2b391267fb42a
Ben Longden <ben.longden@theguardian.com>
2020-12-04 13:26:35 +0000

    added hrh elements

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,42 @@
-// if you want to import a module from shared/js then you can
-// just do e.g. import Scatter from "shared/js/scatter.js"
+function pullContentIntoAtom() {
+	const elAll = contentElements(['p', 'h2', 'figure']);
+
+	if (elAll.length > 0) {
+
+		const moveTarget = document.querySelector('body > .element-atom .body-content');
+		elAll.forEach((el) => {
+			if (el.hasAttribute('data-canonical-url')) {
+				// console.log('this is the element', el)
+				const url = el.dataset.canonicalUrl;
+				let iframeEl = document.createElement('iframe');
+				iframeEl.setAttribute('src', url);
+				// console.log(iframeEl);
+				el.appendChild(iframeEl);
+
+				setTimeout(() => {
+					// console.log('hi');
+				}, 2000);
+
+			}
+			moveTarget.appendChild(el);
+		})
+		document.body.classList.add('DOMready')
+	} else {
+		// console.log('DOM ready');
+	}
+}
+
+pullContentIntoAtom();
+let i = 0;
+const contentInterval = setInterval(function () {
+	pullContentIntoAtom();
+	if (i++ >= 20) clearInterval(contentInterval);
+}, 3000);
+
+
+function contentElements(elements) {
+	const selector = elements.map((el) => {
+		return `body > .element-atom ~ ${el}`;
+	}).join(',');
+	return document.querySelectorAll(selector);
+}
--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -5,18 +5,18 @@ document.body.appendChild(el);
 
 
 setTimeout(() => {
-  if (window.resize) {  
+  if (window.resize) {
     const html = document.querySelector('html')
     const body = document.querySelector('body')
-  
+
     html.style.overflow = 'hidden'
     html.style.margin = '0px'
     html.style.padding = '0px'
-  
+
     body.style.overflow = 'hidden'
     body.style.margin = '0px'
     body.style.padding = '0px'
-  
+
   window.resize()
   }
 },100)
