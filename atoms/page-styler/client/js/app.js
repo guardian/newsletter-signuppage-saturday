62a823d05f15e13f08b6d1c69369dbfbc00c8820
Ben Longden <ben.longden@theguardian.com>
2020-12-11 14:49:35 +0000

    added header

--- atoms/page-styler/client/js/app.js
+++ atoms/page-styler/client/js/app.js
@@ -1,3 +1,5 @@
+import anime from 'animejs/lib/anime.es.js';
+
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
 function pullContentIntoAtom() {
@@ -42,3 +44,66 @@ function contentElements(elements) {
 	}).join(',');
 	return document.querySelectorAll(selector);
 }
+
+console.log(anime, 'anime');
+
+var roundone = document.querySelector('.roundone');
+var roundtwo = document.querySelector('.roundtwo');
+var roundthree = document.querySelector('.roundthree');
+var roundfour = document.querySelector('.roundfour');
+var roundfive = document.querySelector('.roundfive');
+var roundsix = document.querySelector('.roundsix');
+
+anime({
+  targets: roundone,
+  innerHTML: [0, 525600],
+  easing: 'linear',
+  round: 1,
+	delay: 6000,
+	duration: 2000
+});
+
+anime({
+  targets: roundtwo,
+  innerHTML: [0, 8760],
+  easing: 'linear',
+  round: 1,
+	delay: 5000,
+	duration: 1000
+});
+
+anime({
+  targets: roundthree,
+  innerHTML: [0, 365],
+  easing: 'linear',
+  round: 1,
+	delay: 4000,
+	duration: 1000
+});
+
+anime({
+  targets: roundfour,
+  innerHTML: [0, 52],
+  easing: 'linear',
+  round: 1,
+	delay: 3000,
+	duration: 1000
+});
+
+anime({
+  targets: roundfive,
+  innerHTML: [0, 12],
+  easing: 'linear',
+  round: 1,
+	delay: 2000,
+	duration: 1000
+});
+
+anime({
+  targets: roundsix,
+  innerHTML: [0, 1],
+  easing: 'linear',
+  round: 1,
+	delay: 1000,
+	duration: 1000
+});
