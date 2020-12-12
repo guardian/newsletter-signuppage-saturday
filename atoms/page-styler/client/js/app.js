a8a2431b7505697b4c6ebaaaf24eca87bfa663b1
Ben Longden <ben.longden@theguardian.com>
2020-12-12 20:46:58 +0000

    almost finished

--- atoms/page-styler/client/js/app.js
+++ atoms/page-styler/client/js/app.js
@@ -59,7 +59,7 @@ anime({
   innerHTML: [0, 525600],
   easing: 'linear',
   round: 1,
-	delay: 6000,
+	delay: 5500,
 	duration: 2000
 });
 
@@ -68,7 +68,7 @@ anime({
   innerHTML: [0, 8760],
   easing: 'linear',
   round: 1,
-	delay: 5000,
+	delay: 4500,
 	duration: 1000
 });
 
@@ -77,7 +77,7 @@ anime({
   innerHTML: [0, 365],
   easing: 'linear',
   round: 1,
-	delay: 4000,
+	delay: 3500,
 	duration: 1000
 });
 
@@ -86,7 +86,7 @@ anime({
   innerHTML: [0, 52],
   easing: 'linear',
   round: 1,
-	delay: 3000,
+	delay: 2500,
 	duration: 1000
 });
 
@@ -95,7 +95,7 @@ anime({
   innerHTML: [0, 12],
   easing: 'linear',
   round: 1,
-	delay: 2000,
+	delay: 1500,
 	duration: 1000
 });
 
@@ -104,6 +104,100 @@ anime({
   innerHTML: [0, 1],
   easing: 'linear',
   round: 1,
-	delay: 1000,
+	delay: 500,
 	duration: 1000
 });
+
+function insertAfter(newNode, referenceNode) {
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function dateandtime(){
+	var bodyContent = document.querySelector('.body-content');
+	var headers = bodyContent.querySelectorAll('h2');
+
+	console.log(headers.length);
+
+	for(i=0; i < headers.length; i++) {
+		console.log('counting', headers[i].innerHTML);
+		var newel = document.createElement('div');
+		newel.setAttribute('class', 'timecounter');
+
+		//check inner content of the h2
+		if(headers[i].innerHTML.includes("31 December 2019")){
+			var minutes = '<span>' + 1440 + ' minutes' + '</span>';
+			var hours = '<span>' + 24 + ' hours' + '</span>';
+			var days = '<span>' + 1 + ' day' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("20 January 2020")){
+			var minutes = '<span>' + 30+','+240 + ' minutes' + '</span>';
+			var hours = '<span>' + 504 + ' hours' + '</span>';
+			var days = '<span>' + 21 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("15 February 2020")){
+			var minutes = '<span>' + 68+','+680 + ' minutes' + '</span>';
+			var hours = '<span>' + 1128 + ' hours' + '</span>';
+			var days = '<span>' + 47 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("9 March 2020")){
+			var minutes = '<span>' + 100+','+800 + ' minutes' + '</span>';
+			var hours = '<span>' + 1680 + ' hours' + '</span>';
+			var days = '<span>' + 70 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("16 March")){
+			var minutes = '<span>' + 110+','+880 + ' minutes' + '</span>';
+			var hours = '<span>' + 1848 + ' hours' + '</span>';
+			var days = '<span>' + 77 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("7 April 2020")){
+			var minutes = '<span>' + 142+','+560 + ' minutes' + '</span>';
+			var hours = '<span>' + 2376 + ' hours' + '</span>';
+			var days = '<span>' + 99 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("26 June 2020")){
+			var minutes = '<span>' + 257+','+760 + ' minutes' + '</span>';
+			var hours = '<span>' + 4296 + ' hours' + '</span>';
+			var days = '<span>' + 179 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("16 July 2020")){
+			var minutes = '<span>' + 286+','+560 + ' minutes' + '</span>';
+			var hours = '<span>' + 4776 + ' hours' + '</span>';
+			var days = '<span>' + 199 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("2 August 2020")){
+			var minutes = '<span>' + 311+','+'040' + ' minutes' + '</span>';
+			var hours = '<span>' + 5184 + ' hours' + '</span>';
+			var days = '<span>' + 216 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("22 September 2020")){
+			var minutes = '<span>' + 384+','+480 + ' minutes' + '</span>';
+			var hours = '<span>' + 6408 + ' hours' + '</span>';
+			var days = '<span>' + 267 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("2 October 2020")){
+			var minutes = '<span>' + 398+','+800 + ' minutes' + '</span>';
+			var hours = '<span>' + 6648 + ' hours' + '</span>';
+			var days = '<span>' + 277 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("9 November 2020")){
+			var minutes = '<span>' + 453+','+600 + ' minutes' + '</span>';
+			var hours = '<span>' + 7560 + ' hours' + '</span>';
+			var days = '<span>' + 315 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		}else if(headers[i].innerHTML.includes("23 November")){
+			var minutes = '<span>' + 472+','+320 + ' minutes' + '</span>';
+			var hours = '<span>' + 7872 + ' hours' + '</span>';
+			var days = '<span>' + 328 + ' days' + '</span>';
+			newel.innerHTML = minutes + hours + days;
+		} else {
+			newel.innerHTML = "Working out the time";
+		}
+
+		insertAfter(newel , headers[i]);
+	}
+}
+
+setTimeout(function(){
+	console.log('waited');
+	dateandtime();
+}, 3000);
