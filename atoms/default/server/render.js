2b733f18aa030607791f225d9b8b6eee496085a2
Ben Longden <ben.longden@theguardian.com>
2021-06-23 15:12:29 +0100

    adding design interactives code

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,15 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+import mainTemplate from "./atoms/default/server/templates/main.html!text"
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainHTML;
-} 
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1QdaDFk-mB1ccwIgd79TiezBgW_1IaDcv7AB2y0YCQsI.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
+}
